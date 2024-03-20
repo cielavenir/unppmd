c59667560dd30fd1e13378c3ec5e55174ae12895
cielavenir <cielartisan@gmail.com>
2024-03-20 09:50:01 +0900

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-gcc -O2 -ounppmd -DSTANDALONE applet/unppmd.c applet/lzma_ppmd7_includer.c applet/lzma_ppmd7a_includer.c applet/lzma_ppmd8_includer.c lib/popt/*.c
+gcc -O2 -ounppmd -DSTANDALONE applet/unppmd.c applet/lzma_alloc_includer.c applet/lzma_ppmd7_includer.c applet/lzma_ppmd7a_includer.c applet/lzma_ppmd8_includer.c lib/popt/*.c
